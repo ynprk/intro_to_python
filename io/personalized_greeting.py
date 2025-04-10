67d90c1c6f0f54405558e04d529c6b5565e64ae2
Yena Park <yenapark@nyu.edu>
2025-04-09 23:50:04 -0400

    exercises and examples from io

---
+++ io/personalized_greeting.py
@@ -0,0 +1,7 @@
+print("What's your name?")
+name = input()
+print(f'Good Morning, {name}!')
+
+# You can also use the input() function to display the prompt the user sees:
+name = input("What's your name again?\n")
+print(f'Good Morning, {name}!')
