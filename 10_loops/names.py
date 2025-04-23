ee5e72909e8dc894c5c05cba64eddbed0ad63f79
Yena Park <yenapark@nyu.edu>
2025-04-23 17:52:21 -0400

    exercises for loops

---
+++ 10_loops/names.py
@@ -0,0 +1,11 @@
+names = ['Chris', 'Max', 'Karis', 'Victor']
+upper_names = []
+index = 0
+
+while index < len(names):
+    upper_name = names[index].upper()
+    upper_names.append(upper_name)
+    index += 1
+
+print(upper_names)
+# ['CHRIS', 'MAX', 'KARIS', 'VICTOR']
---
+++ 10_loops/names2.py
@@ -0,0 +1,9 @@
+names = ['Chris', 'Max', 'Karis', 'Victor']
+upper_names = []
+
+for name in names:
+    upper_name = name.upper()
+    upper_names.append(upper_name)
+
+print(upper_names)
+# ['CHRIS', 'MAX', 'KARIS', 'VICTOR']
